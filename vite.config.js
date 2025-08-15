4fb0365cd04051b3911d92ac6ef49889f9e6362b
ppsaheed <ppsaheed@gmail.com>
2025-08-15 23:26:42 +0530

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/44-digital-business-services/',
+  base: '/44/',
   build: {
     outDir: 'dist',
   },
