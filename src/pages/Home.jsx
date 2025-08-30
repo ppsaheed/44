4db69e6d5aeb8a8c93a3f4353f151fefecfacf40
ppsaheed <ppsaheed@gmail.com>
2025-08-30 22:09:27 +0530

    Gov image w100md100

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
               <span className="text-gradient">{companyInfo.motto}</span>
             </h1>
             <div className="flex justify-center items-center gap-6 mb-8 animate-fadeInUp animation-delay-300">
-              <img src="/Gov_logo.png" alt="Government Logo 1" className="w-200 md:w-200" />
+              <img src="/Gov_logo.png" alt="Government Logo 1" className="w-100 md:w-100" />
             </div>
             <p className="text-xl text-gray-700 mb-8 animate-fadeInUp animation-delay-200">
               {companyInfo.description}
